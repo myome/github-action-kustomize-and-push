098fc1fa7bd10f9649ad3a063e33edf3ba448b53
Mauricio Wyler <mwyler@ebowe.com>
2021-12-26 22:59:46 -0300

    Fixed incorrect if

--- entrypoint.sh
+++ entrypoint.sh
@@ -50,13 +50,13 @@ git config --global user.name "$USER_NAME"
 	exit 1
 }
 
-if [ -d "$CLONE_DIR/$TARGET_DIRECTORY" ]; then
+if [ ! -d "$CLONE_DIR/$TARGET_DIRECTORY" ]; then
     echo "::error::Requested directory dont' exist: $CLONE_DIR/$TARGET_DIRECTORY"
 	ls -a $CLONE_DIR/$TARGET_DIRECTORY
     exit 1
 fi
 
-if [ -f $CLONE_DIR/$TARGET_DIRECTORY/kustomization.yaml ]; then
+if [ ! -f $CLONE_DIR/$TARGET_DIRECTORY/kustomization.yaml ]; then
     echo "::error::kustomization.yaml dont' exist in $CLONE_DIR/$TARGET_DIRECTORY"
 	exit 1
 fi
